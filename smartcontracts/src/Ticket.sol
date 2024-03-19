66f6f235f00afbef0082d310493e009914cc6f6e
Igor Coraine <igor.coraine@gmail.com>
2024-03-19 19:54:31 -0300

    commiting to install counters.sol

--- smartcontracts/src/Ticket.sol
+++ smartcontracts/src/Ticket.sol
@@ -2,8 +2,16 @@
 pragma solidity ^0.8.20;
 
 import {ERC721} from "../lib/openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
+import "../lib/openzeppelin-contracts/contracts/access/Ownable.sol";
+import "../lib/openzeppelin-contracts/contracts/utils/Counters.sol";
 
-contract MyCollectible is ERC721 {
+contract Ticket is ERC721, Ownable{
+
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
     constructor() ERC721("MyCollectible", "MCO") {
     }
+
+    
 }
